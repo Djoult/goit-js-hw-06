23a062117ab4621e856acac7b5a7c49b266a5810
Djoult <55453354+Djoult@users.noreply.github.com>
2023-02-15 18:48:49 +0200

    Solve tasks 8, 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmitWithFormData);
+
+function handleSubmitWithFormData(event) {
+  event.preventDefault();
+
+  const { elements } = event.currentTarget;
+  //   console.dir(elements);
+
+  const emailInput = elements.email;
+  const passwordInput = elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Fill all fields");
+    return;
+  }
+  const data = {
+    [emailInput.name]: emailInput.value,
+    [passwordInput.name]: passwordInput.value,
+  };
+
+  console.log(data);
+
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector("button.change-color");
+const color = document.querySelector(".color");
+changeColorBtn.addEventListener("click", changeBodyColor);
+function changeBodyColor() {
+  document.body.style.background = getRandomHexColor();
+  color.textContent = document.body.style.background;
+}
